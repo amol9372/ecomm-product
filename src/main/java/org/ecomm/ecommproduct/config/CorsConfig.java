717242c8b7b220dc6c12b91077f62257aa9abf52
amolsingh <amolsingh9372@gmail.com>
2024-02-01 09:08:40 -0500

    gateway jwt token

--- src/main/java/org/ecomm/ecommproduct/config/CorsConfig.java
+++ src/main/java/org/ecomm/ecommproduct/config/CorsConfig.java
@@ -14,28 +14,28 @@ import java.util.Collections;
 @Configuration
 public class CorsConfig extends CorsConfiguration {
 
-  @Bean
-  public CorsFilter filter() {
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    final CorsConfiguration config = new CorsConfiguration();
-
-    config.setAllowCredentials(true);
-    config.setAllowedOriginPatterns(Collections.singletonList("*"));
-    config.addAllowedHeader("origin");
-    config.addAllowedHeader("x-sso-token");
-    config.addAllowedHeader("x-requested-with");
-    config.addAllowedHeader("content-type");
-    config.addAllowedHeader("accept");
-    config.addAllowedHeader("authorization");
-    config.addAllowedHeader("cookie");
-    config.addAllowedHeader("x-correlation-id");
-    config.addAllowedHeader("Access-Control-Allow-Headers");
-    config.addAllowedHeader("Access-Control-Allow-Origin");
-    config.addAllowedHeader("Access-Control-Allow-Credentials");
-    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-    source.registerCorsConfiguration("/**", config);
-    return new CorsFilter(source);
-  }
+//  @Bean
+//  public CorsFilter filter() {
+//    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    final CorsConfiguration config = new CorsConfiguration();
+//
+//    config.setAllowCredentials(true);
+//    config.setAllowedOriginPatterns(Collections.singletonList("*"));
+//    config.addAllowedHeader("origin");
+//    config.addAllowedHeader("x-sso-token");
+//    config.addAllowedHeader("x-requested-with");
+//    config.addAllowedHeader("content-type");
+//    config.addAllowedHeader("accept");
+//    config.addAllowedHeader("authorization");
+//    config.addAllowedHeader("cookie");
+//    config.addAllowedHeader("x-correlation-id");
+//    config.addAllowedHeader("Access-Control-Allow-Headers");
+//    config.addAllowedHeader("Access-Control-Allow-Origin");
+//    config.addAllowedHeader("Access-Control-Allow-Credentials");
+//    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+//    source.registerCorsConfiguration("/**", config);
+//    return new CorsFilter(source);
+//  }
 
 
 }
